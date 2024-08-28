9546c566ba601a53e8356a79adf7095ca6f67c3d
himanshu6388 <himanshuverma8808613782@gmail.com>
2024-08-28 21:42:15 +0530

    uploaded

---
+++ src/components/PokemonList/PokemonList.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import './PokemonList.css';
+import axios from 'axios';
+import Pokemon from "../Pokemon/Pokemon";
+
+function PokemonList(){
+
+    const [pokemonList, setPokemonList] =  useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    
+    async function downlaodPokemons(params) {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
+        const pokemonResults = response.data.results;
+       const pokemonResultsPromise =  pokemonResults.map((pokemon) => axios.get(pokemon.url));
+       const pokemonData = await axios.all(pokemonResultsPromise)
+       console.log(pokemonData);
+       const res = pokemonData.map((pokeData)=>{
+        const pokemon = pokeData.data;
+        return {
+            id:pokemon.id,
+            name: pokemon.name,  
+            image: (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default :  pokemon.sprites.front_shiny,
+            types: pokemon.types
+        }
+       });
+       console.log(res);
+       setPokemonList(res);
+        setIsLoading(false);
+    }
+
+    useEffect(()=>{
+            downlaodPokemons();
+    }, [])
+
+
+    return (
+
+        <div className="pokemon-list-wrapper">
+            <div>Pokemon List</div>
+            {(isLoading) ? 'Loading.......' : 
+                pokemonList.map((p)=> <Pokemon name={p.name} image={p.image} key={p.id} />)
+            }
+        </div>
+    )
+}
+
+export default PokemonList;
